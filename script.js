3dbc62d75c53d61d722860e0f88f2a4127c7e9ca
AlexisPaineman <alexispaineman16@gmail.com>
2024-05-23 03:58:25 -0300

    Subir mi carpeta a Github

---
+++ script.js
@@ -0,0 +1,28 @@
+function investigar() {
+    window.location.href = "situacion_futurista1.html";
+}
+
+function huir() {
+    window.location.href = "situacion_futurista3.html";
+}
+
+function activar() {
+    window.location.href = "situacion_futurista2.html";
+}
+
+function dejar() {
+    window.location.href = "resolucion_futurista2.html";
+}
+
+
+function unirte() {
+    window.location.href = "situacion_futurista5.html";
+}
+
+function rechazar() {
+    window.location.href = "situacion_futurista5.html";
+}
+
+function reiniciar() {
+    window.location.href = "index.html";
+}
